70f7556f6d5b57d8ca1629040255d3a538934e46
Amtomkraftwerk <antonthielmann@t-online.de>
2019-12-17 11:08:08 +0100

    defined functions rotate etc.

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,53 +1,34 @@
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
 import os
 from keras.preprocessing.image import ImageDataGenerator
 
-# load the dataset as with pandas
-os.chdir('C:/Users/anton/Desktop/Statistik_master/Deep learning/Sign Language/Deep learning/Data')
-train = pd.read_csv('sign_mnist_train.csv')
-test = pd.read_csv('sign_mnist_test.csv')
-data = pd.concat([train, test], ignore_index=True)
+def rotation(data_x, data_y, angle, size):
+	rotate = ImageDataGenerator(rotation_range=angle)
+	rotate.fit(data_x)
 
-#Since our target variable are in categorical(nomial) - binarize the labels
-label_binarizer = LabelBinarizer()
-labels = label_binarizer.fit_transform(data['label'].values)
+	for x_rot, y_rot in rotate.flow(data_x, data_y, batch_size=size, shuffle=True):
+		break
+	return x_rot, y_rot
 
-#drop the labels from training dataset - first column
-data.drop('label', axis = 1, inplace = True)
 
-# Reshape the images
-images = data.values
-# split data set into training and test set - 70% - 30%
-x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=0.3, random_state = 101)
+def vertical_shift(data_x, data_y, range, size):
+	vertical = ImageDataGenerator(width_shift_range=range)
+	vertical.fit(data_x)
 
-x_train = x_train / train.values.max()
-x_test = x_test / train.values.max()
+	for x_vert, y_vert in vertical.flow(data_x, data_y, batch_size=size, shuffle=True):
+		break
+	return x_vert, y_vert
 
-x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+def brightness_change(data_x, data_y, range, size):
+	brightness = ImageDataGenerator(brightness_range=range)
+	brightness.fit(data_x)
 
-datagen = ImageDataGenerator(rotation_range=30)
-# fit parameters from data
-datagen.fit(x_train)
-# configure batch size and retrieve one batch of images
-for x_aug, y_aug in datagen.flow(x_train, y_train, batch_size=len(x_test), shuffle=False):
-	break
+	for x_bright, y_bright in brightness.flow(data_x, data_y, batch_size=size, shuffle=True):
+		break
+	return x_bright, y_bright
 
-fig = plt.figure()
-for i in range(1, 10):
-	ax = fig.add_subplot(3, 3, i)
-	ax.imshow(x_train[i].reshape(28, 28), cmap="Greys")
-
-fig = plt.figure()
-for i in range(1, 10):
-	ax = fig.add_subplot(3, 3, i)
-	ax.imshow(x_aug[i].reshape(28, 28), cmap="Greys")
 
 
 
