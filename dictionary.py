75d0813018098f40370d32b7df877a2f05c5183a
JENScoding <jens.lichter@posteo.de>
2020-01-12 17:44:12 +0100

    Extra script for dictionary

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+# dictionary for numbers
+
+import pandas as pd
+import numpy as np
+import string
+
+# load the dataset as with pandas
+train_lab = pd.read_csv("../sign-language-mnist/sign_mnist_train.csv")
+
+# dictonary for numbers of the labels
+un_labels = np.unique(train_lab.iloc[:, 0])
+letters = np.delete(np.array(list(string.ascii_lowercase[0:25])), 9)
+s = pd.Series(letters, index=un_labels)
+dict_letters = s.to_dict()
+
+del letters, s, train_lab, un_labels
