b0fa8d303345c69d2ff1e11d8e8713795addfe62
JENScoding <jens.lichter@posteo.de>
2020-01-19 18:56:47 +0100

    adjust grid run

--- run_grid.sh
+++ run_grid.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd "${0%/*}"
-for l1 in {0,1e-3,1e-5,1e-7,1e-8,1e-10,1e-12}
+for mixl1l2 in {1e-8,1e-10,0}
 do
 	echo "The following runs were tested with l1 = $l1"
-	for l2 in {0,1e-3,1e-5,1e-7,1e-8,1e-10,1e-12}	
+	for lambda in {0.005,0.001,0.0005}	
 	do python keep_it_lower.py --lambda1 "$l1" --lambda2 "$l2" --epochs 30 --keep_prob 1
 		echo "Second parameter was setted to l2 = $l2"
 	done
